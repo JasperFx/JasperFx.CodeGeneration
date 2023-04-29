42bbea4ac526ec6f2bc46a318a7eb0f1b40b9010
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2023-04-29 16:11:43 -0500

    Imported some helper frames for null guards from Wolverine

--- src/JasperFx.CodeGeneration/Frames/IfElseNullGuardFrame.cs
+++ src/JasperFx.CodeGeneration/Frames/IfElseNullGuardFrame.cs
@@ -1,16 +1,21 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using JasperFx.CodeGeneration.Model;
 
 namespace JasperFx.CodeGeneration.Frames;
 
-public class IfNullGuard : Frame
+/// <summary>
+/// Frame that executes different code based on whether or not the original variable
+/// is null or not
+/// </summary>
+public class IfElseNullGuardFrame : Frame
 {
     private readonly Frame[] _existsPath;
     private readonly Frame[] _nullPath;
     private readonly Variable _subject;
 
-    public IfNullGuard(Variable subject, Frame[] nullPath, Frame[] existsPath) : base(nullPath.Any(x => x.IsAsync) ||
+    public IfElseNullGuardFrame(Variable subject, Frame[] nullPath, Frame[] existsPath) : base(nullPath.Any(x => x.IsAsync) ||
         existsPath.Any(x => x.IsAsync))
     {
         _subject = subject;
@@ -64,4 +69,31 @@ public class IfNullGuard : Frame
             }
         }
     }
+    
+    
+    /// <summary>
+    /// Execute a series of inner frames if the specified variable is not null
+    /// </summary>
+    public class IfNullGuardFrame : CompositeFrame
+    {
+        private readonly Variable _variable;
+
+        public IfNullGuardFrame(Variable variable, params Frame[] inner) : base(inner)
+        {
+            _variable = variable ?? throw new ArgumentNullException(nameof(variable));
+            uses.Add(variable);
+
+            Inners = inner;
+        }
+    
+        public IReadOnlyList<Frame> Inners { get; }
+
+        protected override void generateCode(GeneratedMethod method, ISourceWriter writer, Frame inner)
+        {
+            writer.Write($"BLOCK:if ({_variable.Usage} != null)");
+            inner.GenerateCode(method, writer);
+            writer.FinishBlock();
+        }
+    }
+    
 }
