a1970df0932ea831409bd425027942b836f7ecc7
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-09-27 09:06:05 -0500

    new test harness for aspnetcore integration

---
+++ src/WebServiceTarget/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebServiceTarget.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
